5de9b5185318450511098d4b8093f9dc52bef7d1
Shoe2 <andrewelee1983@gmail.com>
2025-10-29 10:11:53 -0700

    more standalone

--- src/app/archidekt-deck-stats/archidekt-deck-stats.component.ts
+++ src/app/archidekt-deck-stats/archidekt-deck-stats.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-archidekt-deck-stats',
   standalone: false,
-  imports: [],
   templateUrl: './archidekt-deck-stats.component.html',
   styleUrl: './archidekt-deck-stats.component.css'
 })
